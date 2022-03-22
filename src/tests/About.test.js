7a11cd83e056d613bf8bcbd0c1c57406f4cd4355
Paulo Porto <prtpj1@gmail.com>
2022-03-22 18:14:22 -0300

    Req 2 done

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
+import { About } from '../components';
+
+test('Tests if there is information about Pokedex on screen', () => {
+  renderWithRouter(<About />);
+
+  const aboutH2 = screen.getAllByRole(
+    'heading',
+    { Level: 2,
+      name: /About Pokédex/i },
+  );
+  const aboutParag1 = screen.queryByText(/This application simulates/i);
+  const aboutParag2 = screen.queryByText(/One can filter Pokémons by type/i);
+  const imgAbout = screen.getByRole('img', { name: /Pokédex/i });
+  // console.log(imgAbout);
+
+  expect(aboutH2).toBeDefined();
+  expect(aboutParag1).toBeDefined();
+  expect(aboutParag2).toBeDefined();
+  expect(imgAbout.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
+
+// ! npx stryker run ./stryker/.conf.json
+
+// ? Teste se a página contém as informações sobre a Pokédex.
+
+// * Teste se a página contém um heading h2 com o texto About Pokédex.
+
+// * Teste se a página contém dois parágrafos com texto sobre a Pokédex.
+
+// ToDo: Teste se a página contém a seguinte imagem de uma Pokédex: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.'
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
@@ -12,27 +11,24 @@ test('Tests if there is nav links at top of screen', () => {
   const linkFavorite = screen.getByRole('link', { name: /favorite/i });
 
   expect(linkHome).toBeInTheDocument();
-  // userEvent.click(linkHome);
   expect(linkAbout).toBeInTheDocument();
-  // userEvent.click(linkAbout);
   expect(linkFavorite).toBeInTheDocument();
-  // userEvent.click(linkFavorite);
 });
 
-// ! npx stryker run ./stryker/Pokedex.conf.json
+// ! npx stryker run ./stryker/.conf.json
 
 // Teste se o topo da aplicação contém um conjunto fixo de links de navegação.
 
-// ? O primeiro link deve possuir o texto Home.
+// O primeiro link deve possuir o texto Home.
 
-// ? O segundo link deve possuir o texto About.
+// O segundo link deve possuir o texto About.
 
-// ? O terceiro link deve possuir o texto Favorite Pokémons.
+// O terceiro link deve possuir o texto Favorite Pokémons.
 
-// Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.
+// TODO: Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.
 
-// Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.
+// TODO: Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.
 
-// Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.
+// TODO:  Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.
 
-// Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.
+// TODO:  Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.
