c28f1b421c23ac772b35cd0c0f17cbd8dcef96f4
Paulo Porto <prtpj1@gmail.com>
2022-03-22 16:58:56 -0300

    Req 1 done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+test('Tests if there is nav links at top of screen', () => {
+  renderWithRouter(<App />);
+
+  const linkHome = screen.getByRole('link', { name: /home/i });
+  const linkAbout = screen.getByRole('link', { name: /about/i });
+  const linkFavorite = screen.getByRole('link', { name: /favorite/i });
+
+  expect(linkHome).toBeInTheDocument();
+  // userEvent.click(linkHome);
+  expect(linkAbout).toBeInTheDocument();
+  // userEvent.click(linkAbout);
+  expect(linkFavorite).toBeInTheDocument();
+  // userEvent.click(linkFavorite);
+});
+
+// ! npx stryker run ./stryker/Pokedex.conf.json
+
+// Teste se o topo da aplicação contém um conjunto fixo de links de navegação.
+
+// ? O primeiro link deve possuir o texto Home.
+
+// ? O segundo link deve possuir o texto About.
+
+// ? O terceiro link deve possuir o texto Favorite Pokémons.
+
+// Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação.
+
+// Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação.
+
+// Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.
+
+// Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida.
---
+++ src/tests/helpers/renderWithRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+const renderWithRouter = (param) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(
+      <Router history={ history }>
+        {' '}
+        {param}
+        {' '}
+      </Router>,
+    ),
+    history,
+  });
+};
+
+export default renderWithRouter;
